9c21fa5ffec0ddbc1b6ade775aab8ffeb87ce82f
adamswonder <adamswill.i.am71002@gmail.com>
2026-03-29 22:54:49 +0300

    add GitHub contributions chart to footer

--- components/footer.js
+++ components/footer.js
@@ -1,12 +1,26 @@
 import { Box, Link, useColorModeValue } from '@chakra-ui/react'
+import dynamic from 'next/dynamic'
+
+const GithubChart = dynamic(() => import('./github-chart'), { ssr: false })
 
 const Footer = () => {
   return (
-    <Box align="center" my={4} p={3} borderRadius="md" bg={useColorModeValue('whiteAlpha.200', 'whiteAlpha.100')} fontSize="sm" opacity={0.8}>
-      <Box mb={2}>
-        &copy; {new Date().getFullYear()} Adams Wonderboy. All Rights Reserved. <Link href="https://www.craftz.dog/" target="_blank" style={{ textDecoration: 'underline' }}>
-          Takuya Matsuyama
-        </Link>
+    <Box my={4}>
+      <GithubChart />
+      <Box
+        align="center"
+        p={3}
+        borderRadius="md"
+        bg={useColorModeValue('whiteAlpha.200', 'whiteAlpha.100')}
+        fontSize="sm"
+        opacity={0.8}
+      >
+        <Box mb={2}>
+          &copy; {new Date().getFullYear()} Adams Wonderboy. All Rights Reserved.{' '}
+          <Link href="https://www.craftz.dog/" target="_blank" style={{ textDecoration: 'underline' }}>
+            Takuya Matsuyama
+          </Link>
+        </Box>
       </Box>
     </Box>
   )
